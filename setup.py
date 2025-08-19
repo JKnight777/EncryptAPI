1fa8955fa4385ea095557090509a125f7b3280d7
JPankey <justice.pankeythompson@gmail.com>
2025-08-19 13:11:12 -0700

    Added README

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
         
         api_path = os.path.join(base_path, "api")
         
-        print(f"Opening {sys.argv[1]} to output to {sys.argv[2]}")
+        print(f"Opening {sys.argv[1]} to output to output.txt")
 
         glove_input_file = sys.argv[1] #"FT_bins/glove.42B.300d.txt"
         
-        word2vec_output_file = sys.argv[2] #"FT_bins/glove.42B.300d.word2vec.txt"
+        word2vec_output_file = "output.txt" #"FT_bins/glove.42B.300d.word2vec.txt"
 
         print('Step 1 of 2: Converting GloVe to word2vec format...')
         glove2word2vec(glove_input_file, word2vec_output_file)
